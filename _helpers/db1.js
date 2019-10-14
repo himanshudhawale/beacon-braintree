7016b04327cc03966c65a1f93d8c31031b566029
himanshudhawale <himanshudhawale2@icloud.com>
2019-10-13 21:12:50 -0400

    a

--- _helpers/db1.js
+++ _helpers/db1.js
@@ -4,5 +4,5 @@ mongoose.connect(process.env.MONGODB_URI || config.connectionString, {  useCreat
 mongoose.Promise = global.Promise;
 
 module.exports = {
-    User: require('../users/user.model')
+    Product: require('../users/product.model')
 };
